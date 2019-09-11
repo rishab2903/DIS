b985fc18a166eea83e07e96ad45f2e00d8576819
rishab2903 <54606079+rishab2903@users.noreply.github.com>
2019-09-10 21:24:52 -0400

    Assignment 1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Practices
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("How many years experience do you have in professional programming?");
+            string input = Console.ReadLine();
+            if (int.Parse(input) != 0)
+            {
+                for (int i = 0; i < (int.Parse(input)); i++)
+                {
+                    Console.WriteLine("You have" + input + "years of experience");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You have zero years of experience");
+            }
+
+
+
+        }
+        
+    }
+}
